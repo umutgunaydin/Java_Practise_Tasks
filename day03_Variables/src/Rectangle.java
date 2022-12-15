19eb52683d6f673a4af691137daa940e60057f2d
Ümüt Günaydın <109280212+umutgunaydin@users.noreply.github.com>
2022-12-15 13:50:05 +0300

    Add files via upload

---
+++ day03_Variables/src/Rectangle.java
@@ -0,0 +1,13 @@
+public class Rectangle {
+    public static void main(String[] args) {
+
+        int width = 5;
+        int length = 9;
+
+        int area = width * length;
+        int decimeter = 2 * (width + length);
+
+        System.out.println("area = " + area);
+        System.out.println("decimeter = " + decimeter);
+    }
+}
---
+++ day03_Variables/src/StodentInfo.java
@@ -0,0 +1,24 @@
+public class StodentInfo {
+    public static void main(String[] args) {
+
+        String studentName = "Brooks",
+                school_name = "Apple",
+                gradeLevel = "Mid Level";
+        //this is an shortcut of declaring multi variable for any type
+
+        int age = 35;
+        char gender = 'M';
+        double gpa = 3.8;
+        boolean isFulltime = false;
+
+        System.out.println("studentName = " + studentName);
+        System.out.println("age = " + age);
+        System.out.println("gender = " + gender);
+        System.out.println("school_name = " + school_name);
+        System.out.println("gradeLevel = " + gradeLevel);
+        System.out.println("gpa = " + gpa);
+        System.out.println("isFulltime = " + isFulltime);
+
+
+    }
+}
