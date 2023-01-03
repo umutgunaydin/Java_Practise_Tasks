e481cb096154c57fdbb691e9a0cace6e01c3c6bb
Ümüt Günaydın <109280212+umutgunaydin@users.noreply.github.com>
2023-01-03 22:31:42 +0300

    Add files via upload

---
+++ day34_OOP_AbstarctionContinue/Airplane.java
@@ -0,0 +1,10 @@
+package day34_OOP_AbstarctionContinue;
+
+import day34_OOP_AbstarctionContinue.animalTask.Flyable;
+
+public class Airplane implements Flyable { // also this can implement Flyable interface
+    @Override
+    public void fly() {
+
+    }
+}
